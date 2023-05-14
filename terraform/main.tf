251280b3f4b59d1935f6e7dda80615a797102d53
morn1n9st4r <a.lepilo.soft@gmail.com>
2023-05-14 20:36:31 +0300

    rule for moving old files to archive

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,3 +6,19 @@ resource "aws_s3_bucket" "s3_steam_json_data" {
         Environment = "Dev"
     }
 }
+
+
+resource "aws_s3_bucket_lifecycle_configuration" "archive_week_old_data" {
+    bucket = aws_s3_bucket.s3_steam_json_data.id
+
+    rule {
+        id      = "move-to-glacier"
+        status  = "Enabled"
+        prefix  = ""
+
+        transition {
+            days          = 7
+            storage_class = "GLACIER"
+        }
+    }
+}
