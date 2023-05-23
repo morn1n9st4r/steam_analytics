2ba60716f49ae8b19855795c06cfa04c68b081f5
morn1n9st4r <a.lepilo.soft@gmail.com>
2023-05-23 23:36:48 +0300

    unnest inner json with tags

--- src/main/scala/ProcessJSON.scala
+++ src/main/scala/ProcessJSON.scala
@@ -1,5 +1,5 @@
 import org.apache.spark.sql.{SparkSession}
-
+import org.apache.spark.sql.functions.{col}
 object ProcessJSON extends App {
 
     val spark = SparkSession.builder()
@@ -9,16 +9,34 @@ object ProcessJSON extends App {
         
     spark.sparkContext
         .hadoopConfiguration
-        .set("fs.s3a.access.key", "your key")
+        .set("fs.s3a.access.key", "key")
 
     spark.sparkContext
         .hadoopConfiguration
-        .set("fs.s3a.secret.key", "your key")
+        .set("fs.s3a.secret.key", "key")
 
     spark.sparkContext
         .hadoopConfiguration
         .set("fs.s3a.endpoint", "s3.amazonaws.com")
     
-    val df = spark.read.json("s3a://steam-json-bucket/steam_simple.json")
-    df.show()
+    val df = spark.read.option("inferSchema", "true")
+                       .json("s3a://steam-json-bucket/steam_complex.json")
+    
+    df.printSchema()
+
+
+    val df_flattened = df.selectExpr("appid","tags.*")
+    df_flattened.registerTempTable("tags")
+    
+    val prefixedData = df_flattened.columns.foldLeft(df_flattened) { (df, column) =>
+        df.withColumnRenamed(column, "tag_" + column)
+    }
+
+
+
+    val newdf = df.drop(col("tags"))
+                  .join(prefixedData, df("appid") === prefixedData("tag_appid"))
+                  .drop(col("tag_appid"))
+    newdf.printSchema()
+    // df.write.parquet("s3a://steam-json-bucket/parquet/steam_complex.parquet")
 }
